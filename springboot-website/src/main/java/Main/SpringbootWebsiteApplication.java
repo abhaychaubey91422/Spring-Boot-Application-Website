45cf20e1b32c6c67f2f525e89b1a995c0c394ec3
ABHAY KUMAR <abhaychaubey91422@gmail.com>
2025-09-24 15:28:10 +0530

    Initial Commit

---
+++ springboot-website/src/main/java/Main/SpringbootWebsiteApplication.java
@@ -0,0 +1,13 @@
+package Main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebsiteApplication.class, args);
+	}
+
+}
